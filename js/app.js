c80057017b2ca94affbea67b3325755a63915c2d
m-royer <prince.snoopy@gmail.com>
2024-03-27 15:48:14 -0700

    mobile readiness

--- js/app.js
+++ js/app.js
@@ -1,2 +1,28 @@
 "use strict";
 
+let menuToggle = document.getElementById("menuToggle");
+let menuItemsUL = document.getElementById("menuItems");
+let menuItemsDIV = document.getElementById("menuDIV");
+let body = document.body;
+menuToggle.addEventListener("click", openMenu);
+body.addEventListener("click", closeMenu);
+let menuItemsArray = Array.from(menuItemsUL.children);
+
+menuItemsArray.forEach((el) => {
+    el.addEventListener("click", closeMenu);
+});
+
+
+
+function openMenu(event) {
+    console.log("opened");
+    menuItemsUL.classList.add("open");
+    // stop the body from processing the click
+    event.stopImmediatePropagation();
+    
+}
+
+function closeMenu(event) {
+    console.log("closed");
+    menuItemsUL.classList.remove("open");
+}
