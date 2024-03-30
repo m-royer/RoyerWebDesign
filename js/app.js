e5535eafd59d731c26a530b935972ffc7cf7f143
m-royer <prince.snoopy@gmail.com>
2024-03-29 20:07:22 -0700

    more responsive now with better text

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 "use strict";
 
 let menuToggle = document.getElementById("menuToggle");
-let menuItemsUL = document.getElementById("menuItems");
-let menuItemsDIV = document.getElementById("menuDIV");
+let navItemsUL = document.getElementById("menuItems");
 let body = document.body;
 menuToggle.addEventListener("click", openMenu);
 body.addEventListener("click", closeMenu);
-let menuItemsArray = Array.from(menuItemsUL.children);
+let menuItemsArray = Array.from(navItemsUL.children);
 
 menuItemsArray.forEach((el) => {
     el.addEventListener("click", closeMenu);
@@ -16,7 +15,7 @@ menuItemsArray.forEach((el) => {
 
 function openMenu(event) {
     console.log("opened");
-    menuItemsUL.classList.add("open");
+    navItemsUL.classList.toggle("open");
     // stop the body from processing the click
     event.stopImmediatePropagation();
     
@@ -24,5 +23,33 @@ function openMenu(event) {
 
 function closeMenu(event) {
     console.log("closed");
-    menuItemsUL.classList.remove("open");
+    navItemsUL.classList.remove("open");
 }
+
+
+
+// scrollyspy nav
+
+const sections = document.querySelectorAll('section');
+const navItems = navItemsUL.children;
+let activeNav = "";
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                activeNav = entry.target.classList[0];
+                document.querySelector("#menuItems li.active").classList.remove("active");
+                document.querySelector(`#menuItems li[data-section="${activeNav}"]`).classList.add("active");
+            }
+        });
+    },
+    { 
+        root: document, 
+        rootMargin: "0px",
+        threshold: 0.2
+    }
+);
+
+sections.forEach((section) => {
+    observer.observe(section);
+});
